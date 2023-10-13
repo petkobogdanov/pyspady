ec34f0a0f16cec56c5d126c99dc6a25c48d3f3b8
michaelpaglia <michaelpaglia02@gmail.com>
2023-10-13 13:02:36 -0400

    start ramu implementation, lots of debugging to do

--- tgsd.py
+++ tgsd.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.io
 from scipy.fftpack import fft
 import scipy.sparse as sp
+from math import gcd, pi
+import cmath
 """""
         self.iter = iter                # 500
         self.K = K                      # 7
@@ -34,9 +36,10 @@ def gen_gft(dict: dict, is_normalized: bool) -> list[np.ndarray]:
     # calculate sum along columns
     D = sp.diags(np.array(adj.sum(axis=0)).flatten())
     L = D - adj # Laplacian matrix
+    # rank(L)=174
     eigenvalues, psi_gft = np.linalg.eig(L.toarray()) 
     # normalize eigenvectors = D^1/2*L*D^1/2
-    if is_normalized: psi_gft = np.dot(np.dot(np.sqrt(D), L), np.sqrt(D))
+    if is_normalized: psi_gft = np.dot(np.dot(np.sqrt(D), L.toarray()), np.sqrt(D))
     return [psi_gft, eigenvalues]
 
 def gen_dft(t: int) -> np.ndarray:
@@ -57,11 +60,44 @@ def gen_rama(t: int, max_period: int):
         max_period (int): Number of max periods
     Returns:
     """
-    
+
+    """""
+    CNA = repmat(CN,floor(rowSize/N),1);
+    CN_cutoff = CN(1:rem(rowSize,N),:);
+    CNA =cat(1,CNA,CN_cutoff);
+    """""
+    A = np.empty((t, 0))
+
+    for n in range(1, max_period+1): # 1:max_period
+        c1 = np.zeros((n, 1), dtype=complex)
+        k_orig = np.arange(1, n+1)
+        k = [k__ for k__ in k_orig if gcd(k__, n) == 1]
+        for n_ in range(n): # goes up to n-1
+            for a in k:
+                c1[n_] = c1[n_] + cmath.exp(1j*2*pi*a*(n_)/n)
+        c1 = np.real(c1)
+
+        k_orig = np.arange(1, n+1)
+        k = [k__ for k__ in k_orig if gcd(k__, n) == 1]
+        CN_col_size = len(k) # size(k, 2) -> number of columns
+        shifted_arrays = []
+        for j in range(1, CN_col_size+1): # 1:CN_col_size
+            c1_circshift = np.roll(c1, j-1, axis=0)
+            shifted_arrays.append(c1_circshift) # CN = cat(2,CN,circshift(c1,(j-1)))
+
+        CN = np.hstack(shifted_arrays) # stack by column
+        CNA = np.full((t // n, 1), CN) # repmat(CN,floor(rowSize/N),1)
+        rem = t % n # rem(rowSize,N)
+        CN_cutoff = CN[1:rem+1, :] # CN(1:rem(rowSize,N),:)
+
+        CNA = np.concatenate((CNA, CN_cutoff), axis=0) # CNA = cat(1,CNA,CN_cutoff)
+
+        A = np.hstack((A, CNA)) # A = cat(2,A,CNA)
 # test against matlab code
 
 mat = load_matrix() # load data
 type = 'rand'
 X_masked = 'X'
-Psi_GFT = gen_gft(mat, False)
+ram = gen_rama(5, 10)
+Psi_GFT = gen_gft(mat, True)
 Psi_DFT = gen_dft(200)
