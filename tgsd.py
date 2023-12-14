a2f06998a23bbcefadbb2e680d6c20c527ae2f2e
michaelpaglia <michaelpaglia02@gmail.com>
2023-12-13 20:20:15 -0500

    update tgsd algo; add 100 iteration test files per Y and W w/ unit tests

--- tgsd.py
+++ tgsd.py
@@ -1,13 +1,13 @@
+import cmath
 import math
-
+from math import gcd, pi
 import numpy as np
 import scipy.io
-from scipy.fftpack import fft
 import scipy.sparse as sp
-from math import gcd, pi
-import cmath
+from scipy.fftpack import fft
 
-from scipy.sparse.linalg import eigsh
+from Y_unittest import TestYConversion
+from W_unittest import TestWConversion
 
 """""
         self.iter = iter                # 500
@@ -30,6 +30,7 @@ def load_matrix() -> dict:
         dict: dictionary with variable names as keys, and loaded matrices as values
     """
     return scipy.io.loadmat('demo_data.mat')
+
 
 def gen_gft(dict: dict, is_normalized: bool) -> list[np.ndarray]:
     """
@@ -136,102 +137,230 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
         Returns:
             True if orthonormal, false otherwise
         """
-        return np.allclose(np.dot(p_psi_or_phi.T, p_psi_or_phi), np.eye(p_psi_or_phi.shape[1]), atol=1e-10)
+        return np.allclose(np.dot(p_psi_or_phi.T.conj(), p_psi_or_phi), np.eye(p_psi_or_phi.shape[1])) and \
+               np.allclose(np.linalg.norm(p_psi_or_phi, axis=0), np.ones(p_psi_or_phi.shape[1]))
+
     def update_d(p_P, p_X, p_mask, p_lambda_3):
         """
         Learns D via P, X, mask, and lambda 3
         Args:
             p_P: ΨYWΦ
-            p_X: Input X
+            p_X: Some temporal graph signal
             p_mask: Indices represent masked indices in D
-            p_lambda_3: Some lambda value
-
+            p_lambda_3: Some sparsity regularization parameter, λ3
         Returns:
             D = (P +𝜆3Ω ⊙ X) ⊘ (I +𝜆3Ω)
         """
-        p_mask, missing_mask, observed_mask = p_mask-1, np.zeros(p_P.shape), np.ones(p_P.shape)
+        p_mask, missing_mask, observed_mask = p_mask - 1, np.zeros(p_P.shape), np.ones(p_P.shape)
         missing_mask[p_mask % p_P.shape[0], p_mask // p_P.shape[0]] = 1
-        return (p_P + p_lambda_3 * (observed_mask-missing_mask) * p_X) / (1 + p_lambda_3 * (observed_mask-missing_mask))
+        return (p_P.astype(np.complex128) + (p_lambda_3 * (observed_mask - missing_mask)) * p_X.astype(
+            np.longdouble)) / (
+                       1 + (p_lambda_3 * (observed_mask - missing_mask)))
+
+    def hard_update_y(p_sigma, p_W, p_X, p_Z, p_psi, p_phi, p_lambda_1, p_Q_1, p_gamma_1, p_rho_1):
+        """
+        Learn Y based on E1: Y = Q1E1QT2.
+        Args:
+            p_sigma: Some identity matrix, σ
+            p_W: Some encoding matrix, W
+            p_X: Some temporal graph signal, X
+            p_Z: Some intermediate variable, Z
+            p_psi: Some dictionary of non-orthogonal atoms, Ψ
+            p_phi: Some time dictionary of atoms, Φ
+            p_lambda_1: Some diagonal non-negative eigenvalue matrix, λ1
+            p_Q_1: Some orthonormal eigenvector matrix, Q1
+            p_gamma_1: Some Lagrangian multiplier, Γ1
+            p_rho_1: Some penalty parameter, ρ1
+
+        Returns:
+            Y = Q1E1QT2
+        """
+        _B = p_sigma.astype(np.longdouble) @ p_W.astype(np.complex128) @ p_phi.astype(np.complex128)
+        _lambda_2, Q_2 = np.linalg.eigh((_B @ _B.conj().T), UPLO='U')
+        Pi = 2 * p_psi.astype(np.longdouble).conj().T @ p_X.astype(np.complex128) @ (
+            _B.astype(np.complex128)).conj().T + p_rho_1 * p_Z + p_gamma_1
+        QPiQ = p_Q_1.conj().T @ Pi @ Q_2
+        diagonal_lambda_1, diagonal_lambda_2 = np.diag(p_lambda_1), np.diag(_lambda_2).conj().T
+        # Create a new matrix of zeros with shape (7, 200)
+        resized_lambda_1 = np.zeros((diagonal_lambda_1.shape[0], diagonal_lambda_2.shape[0]), dtype=p_lambda_1.dtype)
+        # Fill the diagonal of the new matrix with values from _lambda_3
+        resized_lambda_1[:diagonal_lambda_1.shape[0], :diagonal_lambda_2.shape[0]] = diagonal_lambda_1
+        temp0 = 2 * resized_lambda_1 @ diagonal_lambda_2 + rho_2
+        E = QPiQ / temp0
+        return p_Q_1 @ E @ Q_2.conj().T
+
+    def hard_update_w(p_sigma, p_V, p_X, p_Y, p_psi, p_phi, p_lambda_4, p_Q_4, p_gamma_2, p_rho_2):
+        """
+        Learns W where W = Q3E2QT4, where E2(i, j) = [QT3 Π 2Q4]i,j/2[Λ4]ii[Λ3]jj+p2 and
+        (Q3,Λ3 and (Q4,Λ4) are the (eigenvector, eigenvalue) matrices of
+        ATA and ΦΦT , respectively.
+        Args:
+            p_sigma: Some identity matrix, σ
+            p_V: Some intermediate variable, V
+            p_X: Some temporal graph signal, X
+            p_Y: Some encoding matrix, Y
+            p_psi: Some dictionary of atoms, Ψ
+            p_phi: Some non-orthogonal time dictionary of atoms, Φ
+            p_lambda_4: Some diagonal non-negative eigenvalue matrix, λ4
+            p_Q_4: Some orthonormal eigenvector matrix, Q4
+            p_gamma_2: Some Lagrangian multiplier, Γ2
+            p_rho_2: Some penalty parameter, ρ2
+
+        Returns:
+            W = Q3E2QT4
+        """
+        _A = p_psi.astype(np.longdouble) @ p_Y.astype(np.complex128) @ p_sigma.astype(np.longdouble)
+        _lambda_3, Q_3 = np.linalg.eigh((_A.conj().T @ _A), UPLO='U')
+        # Pi=2*A'*X*Phi'+rho_2*V+Gamma_2;
+        Pi = 2 * (_A.astype(np.complex128)).conj().T @ p_X.astype(np.complex128) @ (
+            p_phi.astype(np.complex128)).conj().T + p_rho_2 * p_V + p_gamma_2
+        QPiQ = Q_3.conj().T @ Pi @ p_Q_4
+
+        diagonal_lambda_3, diagonal_lambda_4 = np.diag(_lambda_3), np.diag(p_lambda_4).conj().T
+        # Create a new matrix of zeros with shape (7, 200)
+        resized_lambda_3 = np.zeros((diagonal_lambda_3.shape[0], diagonal_lambda_4.shape[0]), dtype=_lambda_3.dtype)
+        # Fill the diagonal of the new matrix with values from _lambda_3
+        resized_lambda_3[:diagonal_lambda_3.shape[0], :diagonal_lambda_3.shape[0]] = diagonal_lambda_3
+        temp0 = 2 * resized_lambda_3 @ diagonal_lambda_4 + rho_2
+        E = QPiQ / temp0
+        return Q_3 @ E @ Q_4.conj().T
 
     def get_object(p_mask, p_D, p_X, p_phi, p_psi, p_Y, p_sigma, p_W, p_lambda_1, p_lambda_2, p_lambda_3):
         """
         Returns a new object represented by equation X−YΨWΦ‖
         Args:
-            p_mask: Some specified mask
-            p_D: Some specified D
-            p_X: Some specified X
-            p_phi: Some specified Phi dictionary
-            p_psi: Some specified Psi dictionary
-            p_Y: Some specified Y
-            p_sigma: Some specified Sigma
-            p_W: Some specified W
-            p_lambda_1: Some specified lambda 1 value
-            p_lambda_2: Some specified lambda 2 value
-            p_lambda_3: Some specified lambda 2 value
+            p_mask: Some specified indexed mask
+            p_D: Some intermediate variable, D
+            p_X: Some temporal graph signal, X
+            p_phi: Some time dictionary of atoms Phi
+            p_psi: Some dictionary of atoms, Psi
+            p_Y: Some encoding matrix Y
+            p_sigma: Some identity matrix, σ
+            p_W: Some encoding matrix W
+            p_lambda_1: Some sparsity regularization parameter, λ1
+            p_lambda_2: Some sparsity regularization parameter, λ2
+            p_lambda_3: Some sparsity regularization parameter, λ3
 
         Returns:
             New object represented by X−YΨWΦ‖
         """
         # temp=X(:)-D(:);
         # term3=norm(temp(setdiff(1:end,mask)));
+        p_D, p_phi, p_Y, p_W = p_D.astype(np.complex128), p_phi.astype(np.complex128), p_Y.astype(
+            np.complex128), p_W.astype(np.complex128)
+        p_sigma, p_X, p_psi = p_sigma.astype(np.longdouble), p_X.astype(np.longdouble), p_psi.astype(np.longdouble)
 
-        p_mask, missing_mask, observed_mask = p_mask-1, np.zeros(p_X.shape), np.ones(p_X.shape)
-        missing_mask[p_mask % 175, p_mask // 175] = 1
-        term_3 = np.linalg.norm((observed_mask-missing_mask) * (p_D-p_X))
-        #obj_new = norm(D-Psi*Y*Sigma*W*Phi)+lambda_1*norm(Y,1)+lambda_2*norm(W,1)+lambda_3*term3;
-        return np.linalg.norm(p_D - p_psi @ p_Y @ p_sigma @ p_W @ p_phi) \
-                  + p_lambda_1 * np.linalg.norm(p_Y, ord=1) + p_lambda_2 * np.linalg.norm(p_W, ord=1) + p_lambda_3 * term_3
-    if mask.any():
-        # both are orth; masked
-        if is_orthonormal(psi_d) or is_orthonormal(phi_d):
-            n, t = X.shape
-            hold, Y1 = psi_d.shape
-            p, t = phi_d.shape
-            I_2 = np.eye(n, p)
-            W = np.zeros((k, p))
-            W[0, 0] = .000001
-            Y = np.zeros((Y1, k))
-            sigma = np.eye(k, k)
-            V, Z = 0, Y
-            gamma_1, gamma_2 = Y, W
-            obj_old, objs = 0, []
-            I_Y = np.eye((W @ phi_d @ (W @ phi_d).T).shape[0])
-            I_W = np.eye(((psi_d @ Y).T @ psi_d @ Y).shape[0])
+        p_mask, missing_mask, observed_mask = p_mask - 1, np.zeros(p_X.shape), np.ones(p_X.shape)
+        missing_mask[p_mask % p_X.shape[0], p_mask // p_X.shape[0]] = 1
+        term_3 = np.linalg.norm((observed_mask - missing_mask) * (p_D - p_X), ord=2)
+        # obj_new = norm(D-Psi*Y*Sigma*W*Phi)+lambda_1*norm(Y,1)+lambda_2*norm(W,1)+lambda_3*term3;
+        return np.linalg.norm(p_D - p_psi @ p_Y @ p_sigma @ p_W @ p_phi, ord=2) \
+               + p_lambda_1 * np.linalg.norm(np.abs(p_Y), ord=1) + p_lambda_2 * np.linalg.norm(np.abs(p_W),
+                                                                                               ord=1) + p_lambda_3 * term_3
 
-            for i in range(1, 1+iterations):
-                P = psi_d @ Y @ W @ phi_d
-                D = update_d(P, X, mask, lambda_3)
-                #     B=Sigma*W*Phi;
-                #     Y=(2*Psi'*D*B'+rho_1*Z+Gamma_1)*inv(2*(B*B')+rho_1*I_y+exp(-15));
-                B = sigma @ W @ phi_d
-                # Assuming psi_d, D, B, Z, gamma_1, I_Y are defined
+    n, t = X.shape
 
-                Y = (2 * psi_d.T @ D @ B.T + rho_1 * Z + gamma_1) @ np.linalg.pinv(2 * (B @ B.T) + rho_1 * I_Y + math.exp(-15), rcond=1e-15)
-                # Update Z:
-                # h = Y-gamma_1 / rho_1
-                # Z = sign(h).*max(abs(h)-lambda_1/rho_1, 0)
-                h = Y - (gamma_1 / rho_1)
-                Z = np.sign(h) * np.maximum(np.abs(h) - (lambda_1 / rho_1), 0)
-                # A = psi*Y*sigma
-                A = psi_d @ Y @ sigma
+    hold, Y1 = psi_d.shape
+    p, t = phi_d.shape
+    I_2 = np.eye(n, p)
+    W = np.zeros((k, p))
+    W[0, 0] = .000001
+    Y = np.zeros((Y1, k))
+    sigma = np.eye(k, k)
+    sigmaL = sigmaR = np.eye(sigma.shape)
+    V, Z = 0, Y
+    gamma_1, gamma_2 = Y, W
+    obj_old, objs = 0, []
+    I_Y = np.eye((W @ phi_d @ (W @ phi_d).T).shape[0])
+    I_W = np.eye(((psi_d @ Y).T @ psi_d @ Y).shape[0])
+    # [Q_1,Lam_1]=eig(Psi'*Psi);
+    lam_1, Q_1 = np.linalg.eigh((psi_d.astype(np.longdouble).conj().T @ psi_d.astype(np.longdouble)), UPLO='U')
+    lam_4, Q_4 = np.linalg.eigh((phi_d.astype(np.complex128) @ phi_d.astype(np.complex128).conj().T), UPLO='U')
+    XPhiT = X.astype(np.complex128) @ phi_d.astype(np.complex128).conj().T
+    PsiTX = psi_d.astype(np.longdouble).T @ X.astype(np.complex128)
+
+    for i in range(1, 1 + iterations):
+        P = (psi_d.astype(np.longdouble) @ Y.astype(np.complex128) @ W.astype(np.complex128) @ phi_d.astype(
+            np.complex128))
+        D = update_d(P, X, mask, lambda_3)
+        #     B=Sigma*W*Phi;
+        #     Y=(2*Psi'*D*B'+rho_1*Z+Gamma_1)*inv(2*(B*B')+rho_1*I_y+exp(-15));
+        B = sigma.astype(np.longdouble) @ W.astype(np.complex128) @ phi_d.astype(np.complex128)
+        if mask.any():
+            # Both are orth OR Psi is orth, Phi is not orth
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+                Y = (2 * psi_d.conj().T.astype(np.longdouble) @ D.astype(np.complex128) @ B.conj().T + rho_1 * Z.astype(
+                    np.complex128) + gamma_1.astype(np.complex128)) @ np.linalg.pinv(
+                    2 * (B @ B.conj().T) + rho_1 * I_Y + math.exp(-15)).astype(np.complex128)
+            # Phi is orth, Psi is not orth OR Psi is not orth, Phi is not orth
+            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+                Y = hard_update_y(sigma, W, D, Z, psi_d, phi_d, lam_1, Q_1, gamma_1, rho_1)
+        else:
+            # Both are orth OR Psi is orth, Phi is not orth
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+                # Y=(2*PsiTX*B'+rho_1*Z+Gamma_1)*inv(2*(B*B')+rho_1*I_y+exp(-15));
+                Y = (2 * PsiTX.astype(np.complex128) @ (B.astype(np.complex128)).conj().T + rho_1 * Z.astype(np.complex128) + gamma_1) \
+                    @ np.linalg.pinv(2 * (B @ B.conj().T) + rho_1 * I_Y + math.exp(-15)).astype(np.complex128)
+            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (not is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+                Y = hard_update_y(sigma, W, D, psi_d, phi_d, lam_1, Q_1, gamma_1, rho_1)
+
+
+        # test_instance = TestYConversion()
+        # test_instance.test_y_complex_conversion(i, Y)
+        # Update Z:
+        # h = Y-gamma_1 / rho_1
+        # Z = sign(h).*max(abs(h)-lambda_1/rho_1, 0)
+        h = (Y - (gamma_1.astype(np.complex128) / rho_1)).astype(np.complex128)
+        Z = (np.sign(h) * np.maximum(np.abs(h) - (lambda_1 / rho_1), 0)).astype(np.complex128)
+        # A = psi*Y*sigma
+        A = psi_d.astype(np.longdouble) @ Y.astype(np.complex128) @ sigma.astype(np.longdouble)
+        if mask.any():
+            # Both are orthonormal OR Psi is orth, Phi is not
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
                 # W = inv(2*(A')*A + I_W*rho_2) * (2*A'*D*phi'+rho_2*V+gamma_2)
-                W = np.linalg.pinv(2 * A.T @ A + I_W * rho_2, rcond=1e-15) @ (2 * A.T @ D @ phi_d.T + rho_2 * V + gamma_2)
-                # Update V:
-                # h= W-Gamma_2/rho_2;
-                # V = sign(h).*max(abs(h)-lambda_2/rho_2,0);
-                h = W - (gamma_2 / rho_2)
-                V = np.sign(h) * np.maximum(np.abs(h)-(lambda_2/rho_2), 0)
+                W = np.linalg.pinv(2 * A.conj().T @ A + I_W * rho_2).astype(np.complex128) @ (
+                        2 * A.conj().T @ D.astype(np.complex128) @ phi_d.conj().T + rho_2 * V + gamma_2)
+            # Psi is orth, Phi is not orth OR Psi is not orth, Phi is not orth
+            elif (is_orthonormal(psi_d) and not is_orthonormal(phi_d)) or (not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+                W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
+                                  gamma_2, rho_2)
+        else:
+            # Both are orth OR Phi is orth, Psi is not
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+                # W=inv(2*(A')*A + I_w*rho_2)*(2*A'*XPhiT+rho_2*V+ Gamma_2);
+                W = np.linalg.pinv(2 * A.conj().T @ A + I_W * rho_2).astype(np.complex128) @ (
+                    2 * A.conj().T @ XPhiT.astype(np.complex128) + rho_2 * V + gamma_2)
+            # Psi is orth, Phi is not
+            elif (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+                sigma = sigma @ sigmaR
+                W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
+                                  gamma_2, rho_2)
+            elif not is_orthonormal(psi_d) and not is_orthonormal(phi_d):
+                W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
+                                  gamma_2, rho_2)
 
-                gamma_1, gamma_2 = gamma_1 + rho_1*(Z-Y), gamma_2 + rho_2*(V-W)
-                rho_1, rho_2 = min(rho_1*1.1, 1e5), min(rho_2*1.1, 1e5)
-                # Stop condition
-                if i % 25 == 0:
-                    obj_new = get_object(mask, D, X, phi_d, psi_d, Y, sigma, W, lambda_1, lambda_2, lambda_3)
-                    objs = [objs, obj_new]
-                    residual = abs(obj_old-obj_new)
-                    print(f"obj-{i}={obj_new}, residual-{i}={residual}")
-                    if residual < 1e-6: break
-                    else: obj_old = obj_new
+        # test_instance_w = TestWConversion()
+        # test_instance_w.test_w_complex_conversion(i, W)
+
+        # Update V:
+        # h= W-Gamma_2/rho_2;
+        # V = sign(h).*max(abs(h)-lambda_2/rho_2,0);
+        h = W - (gamma_2.astype(np.complex128) / rho_2)
+        V = (np.sign(h) * np.maximum(np.abs(h) - (lambda_2 / rho_2), 0))
+
+        gamma_1, gamma_2 = gamma_1 + rho_1 * (Z - Y), gamma_2 + rho_2 * (V - W)
+        rho_1, rho_2 = min(rho_1 * 1.1, 1e5), min(rho_2 * 1.1, 1e5)
+
+        # Stop condition
+        if i % 25 == 0:
+            obj_new = get_object(mask, D, X, phi_d, psi_d, Y, sigma, W, lambda_1, lambda_2, lambda_3)
+            objs = [objs, obj_new]
+            residual = abs(obj_old - obj_new)
+            print(f"obj-{i}={obj_new}, residual-{i}={residual}")
+            if residual < 1e-6:
+                break
+            else:
+                obj_old = obj_new
     return None
 
 
@@ -240,4 +369,4 @@ ram = gen_rama(5, 10)
 Psi_GFT = gen_gft(mat, False)
 Psi_GFT = Psi_GFT[0]  # eigenvectors
 Psi_DFT = gen_dft(200)
-tgsd(mat['X'], Psi_GFT, Psi_DFT, mat['mask'], None, None, 2, 7, .1, .1, 1, .01, .01)
+tgsd(mat['X'], Psi_GFT, Psi_DFT, mat['mask'], None, None, 100, 7, .1, .1, 1, .01, .01)
