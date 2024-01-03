3d1a3c7720be6de90c3e401f2a55f21b2fa02588
michaelpaglia <michaelpaglia02@gmail.com>
2024-01-03 15:47:07 -0500

    edit test files, begin mdtd

--- tgsd.py
+++ tgsd.py
@@ -3,24 +3,15 @@ import math
 from math import gcd, pi
 import numpy as np
 import scipy.io
+import tensorly as tl
+import sparse
+from tensorly.contrib.sparse import tensor as sp_tensor
 import scipy.sparse as sp
 from scipy.fftpack import fft
 
+import matplotlib.pyplot as plt
 from Y_unittest import TestYConversion
 from W_unittest import TestWConversion
-
-"""""
-        self.iter = iter                # 500
-        self.K = K                      # 7
-        self.lambda_1 = lambda_1        # .1
-        self.lambda_2 = lambda_2        # .1
-        self.lambda_3 = lambda_3        # 1
-        self.rho_1 = self.lambda_1/10   # lambda_1/10
-        self.rho_2 = self.lambda_2/10   # lambda_2/10
-        self.mask = mask                # mask (vectorized location in 'rand' case)
-        self.Psi_orth_1 = Psi_orth_1    # 1
-        self.Psi_orth_2 = Psi_orth_2    # 1
-"""""
 
 
 def load_matrix() -> dict:
@@ -32,16 +23,25 @@ def load_matrix() -> dict:
     return scipy.io.loadmat('demo_data.mat')
 
 
-def gen_gft(dict: dict, is_normalized: bool) -> list[np.ndarray]:
+def load_syn_data() -> dict:
+    """
+    Loads synthetic matrix from file in directory
+    Returns:
+        dict: dictionary with variable names as keys, and loaded matrices as values
+    """
+    return scipy.io.loadmat('syn_data.mat')
+
+
+def gen_gft(p_dict: dict, is_normalized: bool) -> list[np.ndarray]:
     """
     Constructs a PsiGFT from matlab dictionary (for now)
     Args:
-        dict (dict): Given matlab dictionary
+        p_dict (dict): Given matlab dictionary
         is_normalized (bool): Whether the matrix should be normalized
     Returns:
         list[np.ndarray]: list of numpy arrays in form [psi_gft, eigenvalues]
     """
-    adj = dict['adj']  # given adj matrix
+    adj = p_dict['adj']  # given adj matrix
     # print(np.linalg.matrix_rank(adj.toarray()))
     # calculate sum along columns
     D = np.diag(np.array(adj.sum(axis=0)).flatten())
@@ -95,7 +95,7 @@ def gen_rama(t: int, max_period: int):
         k = [k__ for k__ in k_orig if gcd(k__, n) == 1]
         for n_ in range(n):  # goes up to n-1 inclusive
             for a in k:
-                c1[n_] = c1[n_] + cmath.exp(1j * 2 * pi * a * (n_) / n)
+                c1[n_] = c1[n_] + cmath.exp(1j * 2 * pi * a * n_ / n)
         c1 = np.real(c1)
 
         k_orig = np.arange(1, n + 1)
@@ -127,8 +127,7 @@ def gen_rama(t: int, max_period: int):
 
 
 def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
-         termination_cond,
-         fit_tolerance, iterations: int, k: int, lambda_1: int, lambda_2: int, lambda_3: int, rho_1: int, rho_2: int):
+         iterations: int, k: int, lambda_1: int, lambda_2: int, lambda_3: int, rho_1: int, rho_2: int):
     def is_orthonormal(p_psi_or_phi):
         """
         Determines if graph or time series dictionary is orthonormal
@@ -177,17 +176,16 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
         """
         _B = p_sigma.astype(np.longdouble) @ p_W.astype(np.complex128) @ p_phi.astype(np.complex128)
         _lambda_2, Q_2 = np.linalg.eigh((_B @ _B.conj().T), UPLO='U')
-        Pi = 2 * p_psi.astype(np.longdouble).conj().T @ p_X.astype(np.complex128) @ (
-            _B.astype(np.complex128)).conj().T + p_rho_1 * p_Z + p_gamma_1
-        QPiQ = p_Q_1.conj().T @ Pi @ Q_2
-        diagonal_lambda_1, diagonal_lambda_2 = np.diag(p_lambda_1), np.diag(_lambda_2).conj().T
-        # Create a new matrix of zeros with shape (7, 200)
-        resized_lambda_1 = np.zeros((diagonal_lambda_1.shape[0], diagonal_lambda_2.shape[0]), dtype=p_lambda_1.dtype)
-        # Fill the diagonal of the new matrix with values from _lambda_3
-        resized_lambda_1[:diagonal_lambda_1.shape[0], :diagonal_lambda_2.shape[0]] = diagonal_lambda_1
-        temp0 = 2 * resized_lambda_1 @ diagonal_lambda_2 + rho_2
+        _lambda_2 = np.diag(_lambda_2)
+        # Pi=2*Psi'*X*B'+rho_1*Z+Gamma_1;
+        Pi = 2 * p_psi.astype(np.longdouble).T @ p_X.astype(np.complex128) @ (
+            _B.astype(np.complex128)).conj().T + p_rho_1 * p_Z.astype(np.complex128) + p_gamma_1
+
+        QPiQ = p_Q_1.astype(np.longdouble).T @ Pi.astype(np.complex128) @ Q_2.astype(np.complex128)
+        diagonal_lambda_1, diagonal_lambda_2 = np.diag(p_lambda_1)[:, None], (np.diag(_lambda_2)[:, None]).T
+        temp0 = 2 * diagonal_lambda_1.astype(np.longdouble) @ diagonal_lambda_2.astype(np.longdouble) + rho_2
         E = QPiQ / temp0
-        return p_Q_1 @ E @ Q_2.conj().T
+        return p_Q_1.astype(np.longdouble) @ E.astype(np.complex128) @ Q_2.astype(np.complex128).conj().T
 
     def hard_update_w(p_sigma, p_V, p_X, p_Y, p_psi, p_phi, p_lambda_4, p_Q_4, p_gamma_2, p_rho_2):
         """
@@ -211,19 +209,16 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
         """
         _A = p_psi.astype(np.longdouble) @ p_Y.astype(np.complex128) @ p_sigma.astype(np.longdouble)
         _lambda_3, Q_3 = np.linalg.eigh((_A.conj().T @ _A), UPLO='U')
+        _lambda_3 = np.diag(_lambda_3)
         # Pi=2*A'*X*Phi'+rho_2*V+Gamma_2;
         Pi = 2 * (_A.astype(np.complex128)).conj().T @ p_X.astype(np.complex128) @ (
             p_phi.astype(np.complex128)).conj().T + p_rho_2 * p_V + p_gamma_2
-        QPiQ = Q_3.conj().T @ Pi @ p_Q_4
+        QPiQ = Q_3.astype(np.complex128).conj().T @ Pi @ p_Q_4.astype(np.complex128)
 
-        diagonal_lambda_3, diagonal_lambda_4 = np.diag(_lambda_3), np.diag(p_lambda_4).conj().T
-        # Create a new matrix of zeros with shape (7, 200)
-        resized_lambda_3 = np.zeros((diagonal_lambda_3.shape[0], diagonal_lambda_4.shape[0]), dtype=_lambda_3.dtype)
-        # Fill the diagonal of the new matrix with values from _lambda_3
-        resized_lambda_3[:diagonal_lambda_3.shape[0], :diagonal_lambda_3.shape[0]] = diagonal_lambda_3
-        temp0 = 2 * resized_lambda_3 @ diagonal_lambda_4 + rho_2
+        diagonal_lambda_3, diagonal_lambda_4 = np.diag(_lambda_3)[:, None], (np.diag(p_lambda_4)[:, None]).T
+        temp0 = 2 * diagonal_lambda_3.astype(np.longdouble) @ diagonal_lambda_4.astype(np.longdouble) + rho_2
         E = QPiQ / temp0
-        return Q_3 @ E @ Q_4.conj().T
+        return Q_3.astype(np.complex128) @ E.astype(np.complex128) @ Q_4.astype(np.complex128).conj().T
 
     def get_object(p_mask, p_D, p_X, p_phi, p_psi, p_Y, p_sigma, p_W, p_lambda_1, p_lambda_2, p_lambda_3):
         """
@@ -258,26 +253,31 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
                + p_lambda_1 * np.linalg.norm(np.abs(p_Y), ord=1) + p_lambda_2 * np.linalg.norm(np.abs(p_W),
                                                                                                ord=1) + p_lambda_3 * term_3
 
-    n, t = X.shape
-
+    _, t = X.shape
     hold, Y1 = psi_d.shape
     p, t = phi_d.shape
-    I_2 = np.eye(n, p)
     W = np.zeros((k, p))
     W[0, 0] = .000001
-    Y = np.zeros((Y1, k))
+    if (not is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (
+            is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+        Y = np.eye(Y1, k)
+    else:
+        Y = np.zeros((Y1, k))
     sigma = np.eye(k, k)
-    sigmaL = sigmaR = np.eye(sigma.shape)
+    sigmaR = np.eye(sigma.shape[0], sigma.shape[1])
     V, Z = 0, Y
     gamma_1, gamma_2 = Y, W
     obj_old, objs = 0, []
     I_Y = np.eye((W @ phi_d @ (W @ phi_d).T).shape[0])
     I_W = np.eye(((psi_d @ Y).T @ psi_d @ Y).shape[0])
     # [Q_1,Lam_1]=eig(Psi'*Psi);
-    lam_1, Q_1 = np.linalg.eigh((psi_d.astype(np.longdouble).conj().T @ psi_d.astype(np.longdouble)), UPLO='U')
+    lam_1, Q_1 = np.linalg.eigh((psi_d.T @ psi_d), UPLO='U')
     lam_4, Q_4 = np.linalg.eigh((phi_d.astype(np.complex128) @ phi_d.astype(np.complex128).conj().T), UPLO='U')
+    lam_1, lam_4 = np.diag(lam_1), np.diag(lam_4)
     XPhiT = X.astype(np.complex128) @ phi_d.astype(np.complex128).conj().T
     PsiTX = psi_d.astype(np.longdouble).T @ X.astype(np.complex128)
+
+    plt.figure()
 
     for i in range(1, 1 + iterations):
         P = (psi_d.astype(np.longdouble) @ Y.astype(np.complex128) @ W.astype(np.complex128) @ phi_d.astype(
@@ -288,25 +288,29 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
         B = sigma.astype(np.longdouble) @ W.astype(np.complex128) @ phi_d.astype(np.complex128)
         if mask.any():
             # Both are orth OR Psi is orth, Phi is not orth
-            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (
+                    is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
                 Y = (2 * psi_d.conj().T.astype(np.longdouble) @ D.astype(np.complex128) @ B.conj().T + rho_1 * Z.astype(
                     np.complex128) + gamma_1.astype(np.complex128)) @ np.linalg.pinv(
                     2 * (B @ B.conj().T) + rho_1 * I_Y + math.exp(-15)).astype(np.complex128)
             # Phi is orth, Psi is not orth OR Psi is not orth, Phi is not orth
-            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (
+                    not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
                 Y = hard_update_y(sigma, W, D, Z, psi_d, phi_d, lam_1, Q_1, gamma_1, rho_1)
         else:
             # Both are orth OR Psi is orth, Phi is not orth
-            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (
+                    is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
                 # Y=(2*PsiTX*B'+rho_1*Z+Gamma_1)*inv(2*(B*B')+rho_1*I_y+exp(-15));
-                Y = (2 * PsiTX.astype(np.complex128) @ (B.astype(np.complex128)).conj().T + rho_1 * Z.astype(np.complex128) + gamma_1) \
+                Y = (2 * PsiTX.astype(np.complex128) @ (B.astype(np.complex128)).conj().T + rho_1 * Z.astype(
+                    np.complex128) + gamma_1) \
                     @ np.linalg.pinv(2 * (B @ B.conj().T) + rho_1 * I_Y + math.exp(-15)).astype(np.complex128)
-            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (not is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+            elif (is_orthonormal(phi_d) and not is_orthonormal(psi_d)) or (
+                    not is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
                 Y = hard_update_y(sigma, W, D, psi_d, phi_d, lam_1, Q_1, gamma_1, rho_1)
 
-
-        # test_instance = TestYConversion()
-        # test_instance.test_y_complex_conversion(i, Y)
+        test_instance = TestYConversion()
+        ans_y = test_instance.test_y_complex_conversion(i, Y)
         # Update Z:
         # h = Y-gamma_1 / rho_1
         # Z = sign(h).*max(abs(h)-lambda_1/rho_1, 0)
@@ -316,22 +320,25 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
         A = psi_d.astype(np.longdouble) @ Y.astype(np.complex128) @ sigma.astype(np.longdouble)
         if mask.any():
             # Both are orthonormal OR Psi is orth, Phi is not
-            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (
+                    is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
                 # W = inv(2*(A')*A + I_W*rho_2) * (2*A'*D*phi'+rho_2*V+gamma_2)
                 W = np.linalg.pinv(2 * A.conj().T @ A + I_W * rho_2).astype(np.complex128) @ (
                         2 * A.conj().T @ D.astype(np.complex128) @ phi_d.conj().T + rho_2 * V + gamma_2)
             # Psi is orth, Phi is not orth OR Psi is not orth, Phi is not orth
-            elif (is_orthonormal(psi_d) and not is_orthonormal(phi_d)) or (not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+            elif (is_orthonormal(psi_d) and not is_orthonormal(phi_d)) or (
+                    not is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
                 W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
                                   gamma_2, rho_2)
         else:
             # Both are orth OR Phi is orth, Psi is not
-            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
+            if (is_orthonormal(phi_d) and is_orthonormal(psi_d)) or (
+                    is_orthonormal(phi_d) and not is_orthonormal(psi_d)):
                 # W=inv(2*(A')*A + I_w*rho_2)*(2*A'*XPhiT+rho_2*V+ Gamma_2);
                 W = np.linalg.pinv(2 * A.conj().T @ A + I_W * rho_2).astype(np.complex128) @ (
-                    2 * A.conj().T @ XPhiT.astype(np.complex128) + rho_2 * V + gamma_2)
+                        2 * A.conj().T @ XPhiT.astype(np.complex128) + rho_2 * V + gamma_2)
             # Psi is orth, Phi is not
-            elif (is_orthonormal(psi_d) and not is_orthonormal(phi_d)):
+            elif is_orthonormal(psi_d) and not is_orthonormal(phi_d):
                 sigma = sigma @ sigmaR
                 W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
                                   gamma_2, rho_2)
@@ -339,9 +346,12 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
                 W = hard_update_w(sigma, V, D, Y, psi_d.astype(np.longdouble), phi_d.astype(np.complex128), lam_4, Q_4,
                                   gamma_2, rho_2)
 
-        # test_instance_w = TestWConversion()
-        # test_instance_w.test_w_complex_conversion(i, W)
-
+        test_instance_w = TestWConversion()
+        ans_w = test_instance_w.test_w_complex_conversion(i, W)
+        plt.plot(i, ans_y, 'ro', markersize=5)
+        plt.plot(i, ans_w, 'bo', markersize=5)
+        plt.pause(0.1)  # Adjust the pause duration as needed
+        print("---")
         # Update V:
         # h= W-Gamma_2/rho_2;
         # V = sign(h).*max(abs(h)-lambda_2/rho_2,0);
@@ -361,12 +371,87 @@ def tgsd(X: np.ndarray, psi_d: np.ndarray, phi_d: np.ndarray, mask: np.ndarray,
                 break
             else:
                 obj_old = obj_new
+    plt.xlabel('Iteration')
+    plt.ylabel('Values')
+    plt.title('Convergence of Y and W')
+    plt.grid(True)
+    # Display the final plot
+    plt.show()
     return None
 
 
-mat = load_matrix()  # load data
-ram = gen_rama(5, 10)
-Psi_GFT = gen_gft(mat, False)
-Psi_GFT = Psi_GFT[0]  # eigenvectors
-Psi_DFT = gen_dft(200)
-tgsd(mat['X'], Psi_GFT, Psi_DFT, mat['mask'], None, None, 100, 7, .1, .1, 1, .01, .01)
+def mdtm(is_syn, X, mask, phi_type, phi_d, P, lam, rho, K, epsilon, num_modes):
+    def gen_syn_X(p_syn):
+        # Create a list of matrices from the cell array
+        return tl.kruskal_to_tensor((np.ones(p_syn['Kgen'][0, 0]), [matrix for matrix in p_syn['PhiYg'][0]]))
+
+    def gen_syn_lambda_rho(p_syn):
+        syn_lambda = [0.000001 for _ in p_syn['dimension']]
+        syn_rho = [val * 5 for val in syn_lambda]
+        return [syn_lambda, syn_rho]
+
+    if is_syn:
+        s_data = load_syn_data()
+        X = gen_syn_X(s_data)
+        lam, rho = gen_syn_lambda_rho(s_data)
+        phi_type = ["ortho_dic", "ortho_dic", "not_ortho_dic"]
+        phi_d = s_data['Phi']
+        P = s_data['P']
+
+    # this ignores the try statements at initialization in MDTM.m
+    # cast to Double for mask indexing i.e. double_X = double(X)
+    D = X
+    normalize_scaling = np.ones((K, 1))
+    dimensions = X.shape
+    num_iters_check = 10
+    mask_complex = 1
+    [mask_i, mask_j, mask_t] = np.unravel_index(np.array(mask, dtype=np.intp), dimensions)
+    # mask_tensor = sptensor([mask_i', mask_j', mask_t'], 1);
+    # stack indices and create coordinate list sparse tensor, convert to tensor format
+    mask_tensor = sp_tensor(sparse.COO(np.vstack((mask_i, mask_j, mask_t)), np.ones_like(mask_i), shape=dimensions), dtype='float')
+
+    PhiPhiEV = PhiPhiLAM = [None] * num_modes
+    # dictionary decomposition
+    for i in range(num_modes):
+        if phi_type[i] == "not_ortho_dic":
+            # U and V are unitary matrices and S contains singular values as a diagonal matrix
+            U, S_non_diag, V_transpose = np.linalg.svd(phi_d[0, i])
+            S = np.diag(S_non_diag)
+            V = V_transpose.T
+            rows_v, cols_v = V.shape
+            PhiPhiEV[i] = V
+            retained_rank = min(phi_d[0, i].shape)
+            # diag(S(1:retained_rank, 1:retained_rank)).^2
+            PhiPhiLAM[i] = np.diag(np.diag(S[:retained_rank])**2)
+
+    maxiters = 500
+    # 1:num_modes, @(x) isequal(sort(x), 1:num_modes)
+    dimorder_check = lambda x: sorted(x) == list(range(1, num_modes+1))
+    dimorder = np.arange(1, num_modes+1)
+    gamma = [None] * num_modes
+    gamma[dimorder[0]-1] = 0 # indexing
+
+    YPhiInitInner = np.zeros((K, K, num_modes))
+    Yinit = PhiYInit = [None] * num_modes
+    for n in range(len(dimorder)):
+        Yinit[n] = np.random.rand(P[0, n][0, 0], K)
+        PhiYInit[n] = (phi_d[0, n] @ Yinit[n]) if phi_type[n] in ["not_ortho_dic", "ortho_dic"] else Yinit[n]
+        YPhiInitInner[:, :, n] = PhiYInit[n].T @ PhiYInit[n] if phi_type[n] == "not_ortho_dic" else Yinit[n].T @ Yinit[n]
+        gamma[n] = 0
+
+
+    return None
+
+
+mat = load_matrix()
+ram = gen_rama(400, 10)
+mdtm(is_syn=True, X=None, mask=[], phi_type=None, phi_d=None, P=None, lam=None, rho=None, K=10, epsilon=1e-4,
+     num_modes=3)
+
+# Psi_GFT = gen_gft(mat, False)
+# Psi_GFT = Psi_GFT[0]  # eigenvectors
+# Phi_DFT = gen_dft(200)
+# non_orth_psi = Psi_GFT + 0.1 * np.outer(Psi_GFT[:, 0], Psi_GFT[:, 1])
+# non_orth_phi = Phi_DFT + 0.1 * np.outer(Phi_DFT[:, 0], Phi_DFT[:, 1])
+
+# tgsd(mat['X'], Psi_GFT, Phi_DFT, mat['mask'], None, None, 100, 7, .1, .1, 1, .01, .01)
